6a3e7396370d485d69dab93306610bc36933408e
shashi05shankar <shashi05shankar@gmail.com>
2026-07-04 19:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import tempfile
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import (
     GoogleGenerativeAIEmbeddings,
     ChatGoogleGenerativeAI
