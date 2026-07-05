6b5892c605275fc13dedffed18aa35cfafca7259
shashi05shankar <shashi05shankar@gmail.com>
2026-07-05 11:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if uploaded_files:
     chunks = splitter.split_documents(documents)
 
     # Embeddings
-   embeddings = GoogleGenerativeAIEmbeddings(
+    embeddings = GoogleGenerativeAIEmbeddings(
     model="models/gemini-embedding-001"
 )
     # Vector DB
